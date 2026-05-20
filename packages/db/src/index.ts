97e3140cbcdd7126188de057a81287609c97faeb
Julio Zorro IA <ja.zorro11@uniandes.edu.co>
2026-05-19 19:22:33 -0500

    feat: export searchTasksByTag from db package

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -7,5 +7,14 @@ export * from "./queries/tools";
 export * from "./queries/integrations";
 export * from "./queries/telegram";
 export * from "./queries/tool-calls";
-export * from "./queries/scheduled-tasks";
+export {
+  createScheduledTask,
+  claimDueTasks,
+  createTaskRun,
+  completeTaskRun,
+  failTaskRun,
+  getScheduledTask,
+  listScheduledTasksByUser,
+  searchTasksByTag
+} from "./queries/scheduled-tasks.js";
 export * from "./queries/memories";
